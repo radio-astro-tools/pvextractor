d13ef6d2cb29a807e612b10d27e346034a6c2877
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-17 19:14:24 -0400

    fixed which init was edited

--- pvextractor/__init__.py
+++ pvextractor/__init__.py
@@ -2,3 +2,4 @@ from . import utils
 from .pvextractor import extract_pv_slice
 from .pvwcs import pvwcs_from_header
 from .geometry import Path
+from .pvregions import paths_from_regfile, paths_from_region
--- pvextractor/geometry/__init__.py
+++ pvextractor/geometry/__init__.py
@@ -1,3 +1,2 @@
 from .slices import extract_slice
 from .path import Path
-from .pvregions import paths_from_regfile, paths_from_region
