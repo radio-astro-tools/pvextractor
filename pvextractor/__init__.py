d75ccc4f6d0823ceed094a77871cb012dcead3fb
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-05-05 12:04:44 +0200

    Import WCSPath into top-level namespace for API docs

--- pvextractor/__init__.py
+++ pvextractor/__init__.py
@@ -1,5 +1,5 @@
 from . import utils
 from .pvextractor import extract_pv_slice, extract_pv_slice_hdu
 from .pvwcs import pvwcs_from_header
-from .geometry import Path
+from .geometry import Path, WCSPath
 from .pvregions import paths_from_regfile
