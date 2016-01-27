5493c25f65851aa0eba4acd12f1f776ed4322c24
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-01-27 18:16:49 +0100

    add a test for the fix, but realize that it's broken...

--- pvextractor/tests/test_wcspath.py
+++ pvextractor/tests/test_wcspath.py
@@ -14,8 +14,9 @@ def data_path(filename):
 
 
 @pytest.mark.xfail
-def test_wcspath():
-    p1,p2 = pvregions.paths_from_regfile(data_path('tests.reg'))
+@pytest.mark.parametrize('regfile', ('tests.reg', 'tests_fk5.reg'))
+def test_wcspath(regfile):
+    p1,p2 = pvregions.paths_from_regfile(data_path(regfile))
     w = wcs.WCS(fits.Header.fromtextfile(data_path('w51.hdr')))
     p1.set_wcs(w)
 
@@ -24,3 +25,28 @@ def test_wcspath():
                 226.35546, 100.99054)
     xy = (np.array(zip(shouldbe[::2],shouldbe[1::2])) - 1)
     np.testing.assert_allclose(np.array((p1.xy)), xy, rtol=1e-5)
+
+"""
+TODO:
+    fix this test
+
+/Users/adam/anaconda/envs/astropy35/lib/python3.5/site-packages/numpy/core/function_base.py:115: ValueError
+______________________________________________________________________________________________ test_wcspath[tests.reg] _______________________________________________________________________________________________
+
+regfile = 'tests.reg'
+
+    @pytest.mark.parametrize('regfile', ('tests.reg', 'tests_fk5.reg'))
+    def test_wcspath(regfile):
+>       p1,p2 = pvregions.paths_from_regfile(data_path(regfile))
+E       ValueError: too many values to unpack (expected 2)
+
+pvextractor/tests/test_wcspath.py:18: ValueError
+____________________________________________________________________________________________ test_wcspath[tests_fk5.reg] _____________________________________________________________________________________________
+
+regfile = 'tests_fk5.reg'
+
+    @pytest.mark.parametrize('regfile', ('tests.reg', 'tests_fk5.reg'))
+    def test_wcspath(regfile):
+>       p1,p2 = pvregions.paths_from_regfile(data_path(regfile))
+E       ValueError: too many values to unpack (expected 2)
+"""
