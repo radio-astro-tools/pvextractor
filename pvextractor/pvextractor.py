8f71b235983c95caa2a2fa1e7a9878da60c25b2a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-08-31 13:41:47 -0600

    add a warning if the object is probably a spectral cube but the package couldn't be loaded

--- pvextractor/pvextractor.py
+++ pvextractor/pvextractor.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import numpy as np
+import warnings
 
 from astropy import units as u
 from astropy.extern import six
@@ -112,4 +113,7 @@ def _is_spectral_cube(obj):
         from spectral_cube.spectral_cube import BaseSpectralCube
         return isinstance(obj, BaseSpectralCube)
     except ImportError:
+        if 'SpectralCube' in (obj.__class__):
+            warnings.warn("Object appears to be a SpectralCube, but"
+                          " the spectral_cube module could not be loaded")
         return False
