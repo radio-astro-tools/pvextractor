1f914a04adb4ad7d39ca7104e2ea36acc76b18bd
Brigitta Sipocz <bsipocz@gmail.com>
2019-03-03 18:48:34 -0800

    Use LooseVersion to compare version numbers

--- pvextractor/tests/test_gui.py
+++ pvextractor/tests/test_gui.py
@@ -1,11 +1,8 @@
-import numpy as np
-from numpy.testing import assert_allclose
 import pytest
+from distutils.version import LooseVersion
+import matplotlib as mpl
 
-from astropy.io import fits
 
-from ..pvextractor import extract_pv_slice
-from ..geometry.path import Path
 from ..gui import PVSlicer
 
 from .test_slicer import make_test_hdu
@@ -16,11 +13,12 @@ try:
 except ImportError:
     PYQT5OK = False
 
-import matplotlib as mpl
-if mpl.__version__[0] == '2':
-    MPLOK = False
-else:
+
+if LooseVersion(mpl.__version__) < LooseVersion('2'):
     MPLOK = True
+else:
+    MPLOK = False
+
 
 @pytest.mark.skipif('not PYQT5OK or not MPLOK')
 def test_gui():
@@ -29,17 +27,17 @@ def test_gui():
     pv = PVSlicer(hdu, clim=(-0.02, 2))
     pv.show(block=False)
 
-    x = [100,200,220,330,340]
-    y = [100,200,300,420,430]
+    x = [100, 200, 220, 330, 340]
+    y = [100, 200, 300, 420, 430]
 
     for i in range(len(x)):
-        pv.fig.canvas.motion_notify_event(x[i],y[i])
-        pv.fig.canvas.button_press_event(x[i],y[i],1)
+        pv.fig.canvas.motion_notify_event(x[i], y[i])
+        pv.fig.canvas.button_press_event(x[i], y[i], 1)
 
     pv.fig.canvas.key_press_event('enter')
-    pv.fig.canvas.motion_notify_event(310,420)
-    pv.fig.canvas.button_press_event(410,420,1)
+    pv.fig.canvas.motion_notify_event(310, 420)
+    pv.fig.canvas.button_press_event(410, 420, 1)
 
     pv.fig.canvas.draw()
 
-    assert pv.pv_slice.data.shape == (5,2)
+    assert pv.pv_slice.data.shape == (5, 2)
