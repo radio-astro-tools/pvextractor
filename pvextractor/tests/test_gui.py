5cd66754dc4a53bf9e0631733541c2ed5e4be06e
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-09-08 19:00:08 +0100

    Make GUI test more robust to HiDPI displays

--- pvextractor/tests/test_gui.py
+++ pvextractor/tests/test_gui.py
@@ -1,42 +1,28 @@
-import pytest
-from distutils.version import LooseVersion
-import matplotlib as mpl
-
-
+import numpy as np
 from ..gui import PVSlicer
 
 from .test_slicer import make_test_hdu
 
-try:
-    import PyQt5
-    PYQT5OK = True
-except ImportError:
-    PYQT5OK = False
 
-
-if LooseVersion(mpl.__version__) < LooseVersion('2'):
-    MPLOK = True
-else:
-    MPLOK = False
-
-
-@pytest.mark.skipif('not PYQT5OK or not MPLOK')
 def test_gui():
+
     hdu = make_test_hdu()
 
     pv = PVSlicer(hdu, clim=(-0.02, 2))
     pv.show(block=False)
 
-    x = [100, 200, 220, 330, 340]
-    y = [100, 200, 300, 420, 430]
+    xy_data = np.array([[0.0, 0.1, 0.5, 1.0, 0.5],
+                        [0.0, 0.3, 0.4, 0.9, 1.4]]).T
+
+    x, y = pv.ax1.transData.transform(xy_data).T
 
     for i in range(len(x)):
         pv.fig.canvas.motion_notify_event(x[i], y[i])
         pv.fig.canvas.button_press_event(x[i], y[i], 1)
 
     pv.fig.canvas.key_press_event('enter')
-    pv.fig.canvas.motion_notify_event(310, 420)
-    pv.fig.canvas.button_press_event(410, 420, 1)
+    pv.fig.canvas.motion_notify_event(x[-1] - 20, y[-1])
+    pv.fig.canvas.button_press_event(x[-1] - 20, y[-1], 1)
 
     pv.fig.canvas.draw()
 
