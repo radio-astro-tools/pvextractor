b166c9b12d727093071b60e380f52550112b123d
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-17 19:07:56 +0000

    Run tests and doctests separately to avoid segmentation fault

--- pvextractor/tests/test_gui.py
+++ pvextractor/tests/test_gui.py
@@ -11,7 +11,6 @@ from .test_slicer import make_test_hdu
 MPL_LT_31 = LooseVersion(matplotlib.__version__) < LooseVersion('3.1')
 
 
-@pytest.mark.skipif('not MPL_LT_31')
 def test_gui():
 
     # This tests currently segfaults with Matplotlib 3.1 and later
