04493c10b6bf0d30a12b6bd83006b6ee80cff467
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-11-20 11:43:48 -0500

    try passing kwargs to patchcollection too...

--- pvextractor/geometry/path.py
+++ pvextractor/geometry/path.py
@@ -275,7 +275,7 @@ class Path(object):
         else:
             from matplotlib.collections import PatchCollection
             patches = self.to_patches(spacing, wcs=wcs, **kwargs)
-            artist = PatchCollection(patches)
+            artist = PatchCollection(patches, **kwargs)
         return artist
 
     def show_on_axis(self, ax, spacing, **kwargs):
