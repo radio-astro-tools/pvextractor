81650dede8a9007dc8aabb4fcd2b5ae21d2972f4
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-11-16 10:16:38 +0000

    Infrastructure updates: use OpenAstronomy workflows, updated tox configuration, updated minimum Python and dependencies

--- pvextractor/__init__.py
+++ pvextractor/__init__.py
@@ -1,19 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""
-This is an Astropy affiliated package.
-"""
-
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
-if not _ASTROPY_SETUP_:
-
-    from . import utils
-    from .pvextractor import extract_pv_slice
-    from .utils.wcs_slicing import slice_wcs
-    from .geometry import Path, PathFromCenter
-    from .pvregions import paths_from_regfile
+from . import utils
+from .pvextractor import extract_pv_slice
+from .utils.wcs_slicing import slice_wcs
+from .geometry import Path, PathFromCenter
+from .pvregions import paths_from_regfile
