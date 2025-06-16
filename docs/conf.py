3d800c4e422971a6383c583cdb3db2ee69b87e0b
Eric Koch <koch.eric.w@gmail.com>
2025-06-16 16:40:28 -0400

    More switches to toml from setup cfg

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@
 import datetime
 import os
 import sys
+from importlib import metadata
 
 try:
     from sphinx_astropy.conf.v1 import *  # noqa
@@ -35,12 +36,8 @@ except ImportError:
     print('ERROR: the documentation requires the sphinx-astropy package to be installed')
     sys.exit(1)
 
-# Get configuration information from setup.cfg
-from configparser import ConfigParser
-conf = ConfigParser()
+package_info = metadata.metadata("spectral-cube")
 
-conf.read([os.path.join(os.path.dirname(__file__), '..', 'setup.cfg')])
-setup_cfg = dict(conf.items('metadata'))
 
 # -- General configuration ----------------------------------------------------
 
@@ -63,22 +60,18 @@ rst_epilog += """
 # -- Project information ------------------------------------------------------
 
 # This does not *have* to match the package name, but typically does
-project = setup_cfg['name']
-author = setup_cfg['author']
-copyright = '{0}, {1}'.format(
-    datetime.datetime.now().year, setup_cfg['author'])
+project = package_info["Name"]Add commentMore actions
+author = package_info["Author"]
+copyright = "{}, {}".format(datetime.datetime.now().year, package_info["Author"])
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-__import__(setup_cfg['name'])
-package = sys.modules[setup_cfg['name']]
-
 # The short X.Y version.
-version = package.__version__.split('-', 1)[0]
+version = package_info["Version"].split("-", 1)[0]
 # The full version, including alpha/beta/rc tags.
-release = package.__version__
+release = package_info["Version"]
 
 
 # -- Options for HTML output ---------------------------------------------------
@@ -133,22 +126,6 @@ latex_documents = [('index', project + '.tex', project + u' Documentation',
 # (source start file, name, description, authors, manual section).
 man_pages = [('index', project.lower(), project + u' Documentation',
               [author], 1)]
-
-
-## -- Options for the edit_on_github extension ----------------------------------------
-
-if eval(setup_cfg.get('edit_on_github')):
-    extensions += ['astropy_helpers.sphinx.ext.edit_on_github']
-
-    versionmod = __import__(setup_cfg['package_name'] + '.version')
-    edit_on_github_project = setup_cfg['github_project']
-    if versionmod.version.release:
-        edit_on_github_branch = "v" + versionmod.version.version
-    else:
-        edit_on_github_branch = "master"
-
-    edit_on_github_source_root = ""
-    edit_on_github_doc_root = "docs"
 
 # Add SpectralCube intersphinx
 intersphinx_mapping['spectralcube'] = ('http://spectral-cube.readthedocs.org/en/latest/', None)
